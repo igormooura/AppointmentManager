f7479b725dcaf2c3841dd918ddbc74ba2ed17ac5
igormooura <igor.oliveira.moura@gmail.com>
2025-04-25 17:48:19 -0300

    refactor: edited appointmentSchema

--- model/appointment.ts
+++ model/appointment.ts
@@ -5,7 +5,7 @@ const AppointmentSchema = new Schema({
   clientName: { type: String, required: true }, 
   specialty: { type: String, required: true }, 
   dateTime: { type: Date, required: true },
-  status: { type: String, enum: ['SCHEDULED', 'CANCELLED'] }, 
+  status: { type: String, enum: ['SCHEDULED', 'CANCELLED', 'WAITING FOR ANSWER'], default:"WAITING FOR ANSWER" }, 
 }, { timestamps: true });
 
 export const Appointment = mongoose.model('Appointment', AppointmentSchema);
