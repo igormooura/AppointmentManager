10f4490afca597bfcc53dd4a5aaaec628978ceae
igormooura <igor.oliveira.moura@gmail.com>
2025-04-27 21:37:37 -0300

    refactor: notification schema

--- model/notification.ts
+++ model/notification.ts
@@ -1,8 +1,11 @@
 import mongoose, { Schema } from 'mongoose';
 
 const NotificationSchema = new Schema({
+  notificationId: { type: String, required: true, unique: true },
+  clientName: { type: String, required: true },
+  appointmentId: { type: String, required: true },
   message: { type: String, required: true },
-  read: { type: Boolean, default: false },
+  read: { type: Boolean, default: false }, 
 }, { timestamps: true });
 
 export const Notification = mongoose.model('Notification', NotificationSchema);
