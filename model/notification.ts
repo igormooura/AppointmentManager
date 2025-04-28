13d8b6ce9152f33b44f17eceb4afb93b8b92ff69
igormooura <igor.oliveira.moura@gmail.com>
2025-04-27 22:41:00 -0300

    refactor: instead of using uuid for ids, using the _id that mongodb already gives

--- model/notification.ts
+++ model/notification.ts
@@ -1,9 +1,7 @@
 import mongoose, { Schema } from 'mongoose';
 
 const NotificationSchema = new Schema({
-  notificationId: { type: String, required: true, unique: true },
   clientName: { type: String, required: true },
-  appointmentId: { type: String, required: true },
   message: { type: String, required: true },
   read: { type: Boolean, default: false }, 
 }, { timestamps: true });
