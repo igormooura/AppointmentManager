e86a7a921921812b828eaf5cefb6742150583e27
igormooura <igor.oliveira.moura@gmail.com>
2025-04-27 21:50:37 -0300

    feat: adding the get route

--- routes/appointmentRoutes.ts
+++ routes/appointmentRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express"
-import { createAppointment } from "../controller/appointmentController";
+import { createAppointment, getAppointment } from "../controller/appointmentController";
 
 const router = express.Router();
 
 
 router.post("/createAppointment", createAppointment)
+router.get("/getAppointment/:appointmentId", getAppointment);
 
 export default router
