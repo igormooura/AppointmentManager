85666f28273cc210462833218b8585a4ec95273a
Igor Moura <igor.oliveira.moura@gmail.com>
2025-04-29 22:47:03 -0300

    refactor: excluding waiting for answer from status field

--- model/appointment.ts
+++ model/appointment.ts
@@ -4,7 +4,7 @@ const AppointmentSchema = new Schema({
   clientName: { type: String, required: true }, 
   specialty: { type: String, required: true }, 
   dateTime: { type: Date, required: true },
-  status: { type: String, enum: ['SCHEDULED', 'CANCELLED', 'WAITING FOR ANSWER'], default:"WAITING FOR ANSWER" }, 
+  status: { type: String, enum: ['SCHEDULED', 'CANCELLED']}, 
   reminderSent: { type: Boolean, default: false }
 }, { timestamps: true });
 
