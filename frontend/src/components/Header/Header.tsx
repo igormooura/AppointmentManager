3273ee97c87a1c946e20823e61ff50f795bf8372
igormooura <igor.oliveira.moura@gmail.com>
2025-05-06 15:50:05 -0300

    feat: added the header prototype

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { TfiClose, TfiAlignLeft } from "react-icons/tfi";
+
+const Header = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div>
+      <div className="flex justify-between items-center h-24 mx-auto px-4 shadow-md">
+        <h1 className="w-full text-3xl font-bold text-black">
+          Appointment Manager
+        </h1>
+        <ul className="hidden md:flex">
+          <li className="p-4">Home</li>
+          <li className="p-4">Contact Us</li>
+          <li className="p-4">About</li>
+        </ul>
+
+        <div onClick={handleClick} className="block md:hidden cursor-pointer">
+          {nav ? <TfiClose size={20} /> : <TfiAlignLeft size={20} />}
+        </div>
+      </div>
+
+      <ul
+        className={
+          nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-[#e0e0dac4] bg-white ease-in-out duration-700 md:hidden"
+            : "fixed left-[-100%] ease-in-out duration-500 md:hidden"
+        }
+      >
+        <li className="p-4 border-b border-gray-600">Home</li>
+        <li className="p-4 border-b border-gray-600">Contact Us</li>
+        <li className="p-4 border-b border-gray-600">About</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
