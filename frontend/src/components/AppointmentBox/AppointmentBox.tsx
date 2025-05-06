01543c526a5dae2df79a247baa9d56c4587d0066
igormooura <igor.oliveira.moura@gmail.com>
2025-05-06 17:33:01 -0300

    feat: start appointmentBox

---
+++ frontend/src/components/AppointmentBox/AppointmentBox.tsx
@@ -0,0 +1,9 @@
+const AppointmentBox = () => {
+  return (
+    <div className="max-w-[1240px] mx-auto px-4 flex justify-center items-center">
+      <div className="w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-5xl h-[650px] sm:h-[550px] md:h-[600px] lg:h-[650px] bg-gray-200 shadow-xl flex flex-col p-4 my-4 rounded-lg"></div>
+    </div>
+  );
+};
+
+export default AppointmentBox;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from '../components/Header/Header';
+import AppointmentBox from '../components/AppointmentBox/AppointmentBox';
 
 const Home: React.FC = () => {
     return (
         <div>
             <Header/>
-           
+            <AppointmentBox/>
         </div>
     );
 };
