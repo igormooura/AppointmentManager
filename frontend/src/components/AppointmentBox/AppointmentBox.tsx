5a1633e491b1f183203dac725363577fc3057148
igormooura <igor.oliveira.moura@gmail.com>
2025-05-07 17:18:50 -0300

    chore: added shadcn

--- frontend/src/components/AppointmentBox/AppointmentBox.tsx
+++ frontend/src/components/AppointmentBox/AppointmentBox.tsx
@@ -1,7 +1,20 @@
+import { Calendar } from "@/components/ui/calendar";
+import React from "react";
+
 const AppointmentBox = () => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+
   return (
     <div className="max-w-[1240px] mx-auto px-4 flex justify-center items-center">
-      <div className="w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-5xl h-[650px] sm:h-[550px] md:h-[600px] lg:h-[650px] bg-gray-200 shadow-xl flex flex-col p-4 my-4 rounded-lg"></div>
+      <div className="w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-5xl 
+                      h-[650px] sm:h-[550px] md:h-[600px] lg:h-[650px] bg-gray-200 shadow-xl flex flex-col p-4 my-4 rounded-lg">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border"
+        />
+      </div>
     </div>
   );
 };
