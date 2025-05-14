919b64fb61ce07d1472cd4b767a84ef05d33ea70
Igor Moura <igor.oliveira.moura@gmail.com>
2025-05-14 20:58:20 -0300

    appointmentbox component

--- frontend/src/components/AppointmentBox/AppointmentBox.tsx
+++ frontend/src/components/AppointmentBox/AppointmentBox.tsx
@@ -1,10 +1,16 @@
+import { motion } from "framer-motion";
 
 const AppointmentBox = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <motion.div
+      className="w-full max-w-3xl bg-white shadow-xl rounded-2xl p-8"
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4 }}
+    >
+     
+    </motion.div>
+  );
+};
 
-export default AppointmentBox
+export default AppointmentBox;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import AppointmentBox from "../components/AppointmentBox/AppointmentBox";
+import Background from "@/components/Background/Background";
 
 const Home: React.FC = () => {
   return (
-    <div className="min-h-screen bg-[#FAF9F6]">
+   <Background>
       <Header />
       <AppointmentBox />
-    </div>
+   </Background>
+
   );
 };
 
