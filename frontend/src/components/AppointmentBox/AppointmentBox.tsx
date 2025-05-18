3867a42e23552209eaf9d346e581581e7bcab9ab
Igor Moura <igor.oliveira.moura@gmail.com>
2025-05-17 22:17:09 -0300

    feat: added lastName // center the inputs

--- frontend/src/components/AppointmentBox/AppointmentBox.tsx
+++ frontend/src/components/AppointmentBox/AppointmentBox.tsx
@@ -1,44 +1,50 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
-import Input from "../Inputs/input";
+import Input from "../Inputs/Input";
 
 const AppointmentBox = () => {
   const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [specialty, setSpecialty] = useState("");
 
   const specialtyOptions = [
     { value: "cardiology", label: "Cardiology" },
     { value: "dermatology", label: "Dermatology" },
     { value: "orthopedics", label: "Orthopedics" },
-
   ];
 
   return (
     <div className=" mx-auto flex justify-center items-center min-h-screen">
       <motion.div
-        className=" relative w-7xl h-[600px] bg-white shadow-xl rounded-2xl p-8"
+        className=" relative w-7xl h-[600px] bg-white shadow-xl rounded-2xl p-8 flex flex-col justify-center"
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.4 }}
       >
-        <div className="m-4 flex flex-col gap-4 items-start">
-        <Input
-          placeholder="Your name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Input
-          type="select"
-          value={specialty}
-          onChange={(e) => setSpecialty(e.target.value)}
-          placeholder="Select a specialty"
-          options={specialtyOptions}
-        />
+        <div className="m-4 flex flex-col gap-4 items-start ">
+          <Input
+            placeholder="Your name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            placeholder="Last Name"
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <Input
+            type="select"
+            value={specialty}
+            onChange={(e) => setSpecialty(e.target.value)}
+            placeholder="Select a specialty"
+            options={specialtyOptions}
+          />
         </div>
-    
-   <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[60%] w-px bg-gray-300"/>
 
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[60%] w-px bg-gray-300" />
+        
       </motion.div>
     </div>
   );
