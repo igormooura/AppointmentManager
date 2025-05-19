58b010c71b93ac37e7c837655ac076d17500abd1
Igor Moura <igor.oliveira.moura@gmail.com>
2025-05-18 22:20:00 -0300

    feat: adding http call on schedule button

--- frontend/src/components/Buttons/Schedule.tsx
+++ frontend/src/components/Buttons/Schedule.tsx
@@ -1,7 +1,44 @@
-const Schedule = () => {
+import axios from "axios";
+
+interface ScheduleProps {
+  name: string;
+  lastName: string;
+  email: string;
+  specialty: string;
+  selectedDate: Date;
+}
+
+const Schedule = ({
+  name,
+  lastName,
+  email,
+  specialty,
+  selectedDate,
+}: ScheduleProps) => {
+  const handleSchedule = async () => {
+    const appointmentData = {
+      name,
+      lastName,
+      email,
+      specialty,
+      dateTime: selectedDate.toISOString().split("T")[0],
+    };
+
+    try {
+      const response = await axios.post("http://localhost:3000/createAppointment", appointmentData);
+      console.log( response.data);
+    } catch (error) {
+      console.error(error);
+     
+    }
+  };
+
   return (
     <div className="mt-6 flex justify-center">
-      <button className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
+      <button
+        onClick={handleSchedule}
+        className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+      >
         Schedule Appointment
       </button>
     </div>
