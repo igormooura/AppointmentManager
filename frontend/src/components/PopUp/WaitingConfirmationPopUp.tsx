9532a636d5a3459fcfd10028798b0d44901e5104
igormooura <igor.oliveira.moura@gmail.com>
2025-06-04 16:41:46 -0300

    feat confirmation pop up

--- frontend/src/components/PopUp/WaitingConfirmationPopUp.tsx
+++ frontend/src/components/PopUp/WaitingConfirmationPopUp.tsx
@@ -1,10 +1,11 @@
 const WaitingConfirmationPopUp = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-black opacity-70">
-      <div className=" h-[200px] bg-gray-400 w-[525px] rounded-md">
-        <div className="flex justify-center items-center">
-            <p className=" text-white font-light "> Waiting confirmation from admin...</p>
-        </div>
+    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-70 z-50">
+      <div className="h-[200px] bg-gray-700 w-[525px] rounded-md flex flex-col justify-center items-center p-4">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-white mb-4"></div>
+        <p className="text-white font-light text-xl">
+          Waiting confirmation from admin...
+        </p>
       </div>
     </div>
   );
