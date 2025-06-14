435576486f6ca6834a6961a8850549559b0324bc
Igor Moura <igor.oliveira.moura@gmail.com>
2025-06-14 19:09:13 -0300

    feat: connection to mongodb and rabbitmq

---
+++ backend/config/rabbitmq.ts
@@ -0,0 +1,15 @@
+import amqp, { Channel } from "amqplib";
+
+let channel: Channel;
+
+export const connectToRabbitMQ = async (): Promise<void> => {
+  try {
+    const connect = await amqp.connect("amqp://localhost");
+    channel = await connect.createChannel();
+    await channel.assertQueue("appointment.created");
+    await channel.assertQueue("appointment.status.updated");
+  } catch (error) {
+    console.error("RabbitMQ connection error:", error);
+    process.exit(1);
+  }
+};
