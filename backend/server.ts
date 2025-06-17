f63d7a8ab065d574018efed3633e8bd49982ad96
igormooura <igor.oliveira.moura@gmail.com>
2025-06-17 17:15:08 -0300

    add start server func

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import { createServer } from 'node:http';
 import connectToMongoDb from './config/connectToMongoDb';
 import { connectToRabbitMQ } from './config/rabbitmq';
+import router from './routes/routes';
 
 configDotenv();
 
@@ -13,7 +14,20 @@ const server = createServer(app);
 app.use(cors());
 app.use(express.json());
 
-server.listen(3000, async() => {
-    connectToMongoDb();
-    connectToRabbitMQ();
-});
+app.use(router)
+
+async function startServer() {
+  try {
+    await connectToMongoDb();
+    await connectToRabbitMQ();
+
+    server.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+}
+
+startServer();
