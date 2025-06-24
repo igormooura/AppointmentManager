8bbdf7a59477b2325de112697db6687bd5acad81
igormooura <igor.oliveira.moura@gmail.com>
2025-06-24 15:39:18 -0300

    fix: appointment controller working

--- backend/controllers/appointmentController.ts
+++ backend/controllers/appointmentController.ts
@@ -37,7 +37,7 @@ export const updateAppointmentController = async (req: Request, res: Response): 
 
         const { name, lastName, email, specialty, date, hour, status } = body;
 
-        if (!name || !lastName || !email || !specialty || !date || !hour || !status) {
+        if (!name && !lastName && !email && !specialty && !date && !hour && !status) {
             res.status(400).json({ message: "Missing required appointment fields" });
             return;
         }
