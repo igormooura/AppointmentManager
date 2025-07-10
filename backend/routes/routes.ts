48635db7928713a2a1c0df0983f70728341b03d4
Igor Moura <igor.oliveira.moura@gmail.com>
2025-07-10 16:43:20 -0300

    refactor: appointment by user

--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -2,16 +2,19 @@ import { Router } from "express";
 import {
     createAppointmentController,
     getAllAppointmentsController,
+    getAppointmentByUserController,
     updateAppointmentController
 } from "../controllers/appointmentController";
 
 import { createNotificationController } from "../controllers/notificationController";
+
 
 const router = Router();
 
 router.post("/create-appointment", createAppointmentController);
 router.get("/all-appointments", getAllAppointmentsController);
 router.put("/update-appointment/:_id", updateAppointmentController);
+router.get("/appointment/:email", getAppointmentByUserController);
 
 router.post("/notifications", createNotificationController);
 
