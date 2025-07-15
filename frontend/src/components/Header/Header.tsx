60f754da83b881072ae1e98ef20fa809091748a5
Igor Moura <igor.oliveira.moura@gmail.com>
2025-07-14 23:32:58 -0300

    refactor: now the header have directors

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { TfiClose, TfiAlignLeft } from "react-icons/tfi";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [nav, setNav] = useState(false);
@@ -11,12 +12,19 @@ const Header = () => {
   return (
     <div className="flex justify-between items-center h-24 mx-auto px-4 bg-[#FAF9F6]">
       <h1 className="w-full text-3xl font-bold text-black">
-        Appointment Manager
+        <Link to="/">Appointment Manager</Link>
       </h1>
+
       <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
-        <li className="p-4">Contact Us</li>
-        <li className="p-4">About</li>
+        <li className="p-4">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="p-4">
+          <Link to="/login">Appointments</Link>
+        </li>
+        <li className="p-4">
+          <Link to="/about">About</Link>
+        </li>
       </ul>
 
       <div onClick={handleClick} className="block md:hidden cursor-pointer">
@@ -30,9 +38,15 @@ const Header = () => {
             : "fixed left-[-100%] ease-in-out duration-500 md:hidden"
         }
       >
-        <li className="p-4 border-b border-gray-600">Home</li>
-        <li className="p-4 border-b border-gray-600">Contact Us</li>
-        <li className="p-4 border-b border-gray-600">About</li>
+        <li className="p-4 border-b border-gray-600">
+          <Link to="/" onClick={handleClick}>Home</Link>
+        </li>
+        <li className="p-4">
+          <Link to="/login">Appointments</Link>
+        </li>
+        <li className="p-4 border-b border-gray-600">
+          <Link to="/about" onClick={handleClick}>About</Link>
+        </li>
       </ul>
     </div>
   );
