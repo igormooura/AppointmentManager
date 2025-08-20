632775a049da1b63aaffbf628bdd56dc17052d57
Igor Moura <igor.oliveira.moura@gmail.com>
2025-08-19 22:30:37 -0300

    feat: now the userbox receive an email

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -1,14 +1,30 @@
-import Background from '@/components/Background/Background'
-import UserBox from '@/components/Boxes/UserBox'
-import Header from '@/components/Header/Header'
+import Background from '@/components/Background/Background';
+import UserBox from '@/components/Boxes/UserBox';
+import Header from '@/components/Header/Header';
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const UserPage = () => {
+  const [email, setEmail] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedEmail = localStorage.getItem("userEmail");
+    if (!storedEmail) {
+      navigate("/login"); 
+    } else {
+      setEmail(storedEmail);
+    }
+  }, [navigate]);
+
+  if (!email) return <div>Loading...</div>;
+
   return (
     <Background>
-        <Header/>
-        <UserBox/>
+      <Header />
+      <UserBox email={email} />
     </Background>
-  )
-}
+  );
+};
 
-export default UserPage
+export default UserPage;
